e12083ff73555a077923dd7021a28881ee07bfa4
Raoul Schorer <raoul.schorer@gmail.com>
2015-07-28 13:23:16 +0200

    All is working ok, now compiling with nuitka

--- quest_by_quest.py
+++ quest_by_quest.py
@@ -64,6 +64,7 @@ class Logic:
 class MainWindow(Tk):
     def __init__(self):
         Tk.__init__(self)
+        self.wm_title("FRCA QBase Reader - Single Question Mode")
         w, h = self.winfo_screenwidth(), self.winfo_screenheight()
         self.geometry("%dx%d+0+0" % (w, h))
         self.bind("<Down>", self.OnSmaller)
--- starter.py
+++ starter.py
@@ -6,25 +6,24 @@ from subprocess import call
 
 class StarterWindow(Tk):
     def __init__(self):
-        Tk.__init__(self)
-        self.exambtn = Button(self, text='start an exam',
-                              command=self.startExam)
-        self.exambtn.pack()
-        self.maxquestvar = IntVar()
-        self.maxquest = Spinbox(self, from_=1, to=1667, width=4,
-                                textvariable=self.maxquestvar)
-        self.maxquest.pack()
-        self.questbtn = Button(self, text='start single questions',
-                               command=self.startQuestions)
-        self.questbtn.pack()
-        self.um = '''User manual:\n
-        Start either an exam or single questions\n
-        Go to next question either with mouse or the <right arrow>\n
-        Toggle checkboxes either with mouse or keypad buttons 1-6\n
-        In single question mode, show answer either with mouse or <return>\n
-        In exam mode, results will display after the set question number'''
-        self.usermanual = Label(self, text=self.um, justify=LEFT)
-        self.usermanual.pack()
+		Tk.__init__(self)
+		self.wm_title("FRCA QBase Reader - Start Menu")
+		self.exambtn = Button(self, text='start an exam',command=self.startExam)
+		self.exambtn.pack()
+		self.maxquestvar = IntVar()
+		self.maxquest = Spinbox(self, from_=1, to=1667, width=4,textvariable=self.maxquestvar)
+		self.maxquest.pack()
+		self.questbtn = Button(self, text='start single questions',command=self.startQuestions)
+		self.questbtn.pack()
+		self.um = '''User manual:\n
+		Start either an exam or single questions\n
+		Go to next question either with mouse or the <right arrow>\n
+		Toggle checkboxes either with mouse or <keyboard buttons 1-5>\n
+		In single question mode, show answer either with mouse or <return>\n
+		In exam mode, results will display after the set question number\n
+		Text can be made bigger/smaller with <Up> or <Down> keyboard arrows'''
+		self.usermanual = Label(self, text=self.um, justify=LEFT)
+		self.usermanual.pack()
 
     def startExam(self):
         call(["python","exam.py", str(self.maxquestvar.get())])
